430dbdae696ad4286ad0f693a48ca8d21ce58478
Takayuki Uchida <uchida.takayuki@tis.co.jp>
2023-07-20 17:56:57 +0900

    doc: レビュー指摘対応

--- src/main/java/com/nablarch/example/code/CodeEnum.java
+++ src/main/java/com/nablarch/example/code/CodeEnum.java
@@ -1,6 +1,5 @@
 package com.nablarch.example.code;
 
-import nablarch.core.validation.ee.EnumElement;
 import nablarch.core.validation.ee.EnumElement.WithValue;
 
 /**
@@ -17,7 +16,7 @@ public interface CodeEnum extends WithValue<String> {
 
     /**
      * コード値を返却する。
-     * また、{@link EnumElement}で許容する列挙値を取得するためにも使用する。
+     * また、{@link WithValue}で許容する列挙値を取得するためにも使用する。
      * @return コード値
      */
     @Override
